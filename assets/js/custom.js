cc1f217673e0289c67368eb1031c17a176bcdba2
abu-tal-ha <mabutalha468@gmail.com>
2025-03-24 22:28:46 +0600

    responsive bar icon click done js

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,10 +1,6 @@
+const navBars = document.getElementById("nav-bars");
+  const linkNav = document.getElementById("link-nav");
 
-// JavaScript 
-const navBars = document.querySelector('.nav-bars');
-const linkNav = document.querySelector('.link-nav');
-const iconNav = document.querySelector('.icon-nav');
-
-navBars.addEventListener('click', () => {
-    linkNav.classList.toggle('active');
-    iconNav.classList.toggle('active');
-});
+  navBars.addEventListener("click", () => {
+    linkNav.classList.toggle("active");
+  });
